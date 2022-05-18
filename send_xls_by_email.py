02fa552349043db36fb3dc299804ce0eef8e8154
DELOOZ Olivier <olivier.delooz@atradius.com>
2022-05-18 14:39:25 +0200

    Sends XLS file attached to an email

---
+++ send_xls_by_email.py
@@ -0,0 +1,27 @@
+import platform
+import configparser
+import sys
+import os
+
+from datetime import date, datetime
+import xlsxwriter
+
+from ftplib import FTP
+from pathlib import Path
+
+# import email
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from subprocess import Popen, PIPE
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+def main():
+    print('')
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+    main()
