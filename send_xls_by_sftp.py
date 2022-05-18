e9da33656ea51185908cee4002df7b76d2c283bb
DELOOZ Olivier <olivier.delooz@atradius.com>
2022-05-18 14:34:28 +0200

    Send Excel file via SFTP

---
+++ send_xls_by_sftp.py
@@ -0,0 +1,20 @@
+import platform
+import configparser
+import sys
+import os
+
+from datetime import date, datetime
+import xlsxwriter
+
+from ftplib import FTP
+from pathlib import Path
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+def main():
+    print('')
+    
+
+# ----------------------------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+    main()
